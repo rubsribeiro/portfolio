11f9ceb8431cd3f7d209acd9c12705a5cb181487
Rubia <rubia.n.ribeiro@gmail.com>
2023-08-26 22:17:47 -0300

    atualização arquivo json, javascript e styles

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,65 @@
-fetch('./projects.json').then(response => response.json()).then(data => {
-    console.log(data)
-})
+const elemProjects = document.getElementById('project__content')
+
+const createImage = (projectImage) => {
+    const elemPicture = document.createElement('picture')
+    const elemImg = document.createElement('img')
+
+    elemImg.setAttribute('src', projectImage)
+
+    elemPicture.appendChild(elemImg)
+
+    return elemPicture
+}
+
+const createStrong = (projectName) => {
+    const elemStrong = document.createElement('strong')
+    elemStrong.innerText = projectName
+
+    return elemStrong
+}
+
+const createTags = (projectTags) => {
+    const elemTags = document.createElement('div')
+    projectTags.forEach (tag => {
+        const elemTag = document.createElement('span')
+        elemTag.innerText = tag
+
+        elemTags.appendChild(elemTag)
+    })
+
+    return elemTags
+
+}
+
+const createProject = (project) => {
+    const elemProject = document.createElement('a')
+
+    elemProject.setAttribute('href', project.link)
+    elemProject.setAttribute('target', '_blank')
+
+    elemProject.classList.add('project')
+
+    //add picture
+
+    elemProject.appendChild(createImage(project.image))
+
+    //add strong
+    elemProject.appendChild(createStrong(project.name))
+
+    //add tags
+    elemProject.appendChild(createTags(project.tags))
+
+    return elemProject
+    
+}
+
+const loadProject = (projects) => {
+    projects.forEach(project => {
+        elemProjects.appendChild(createProject(project))
+    })
+}
+
+
+fetch('./projects.json').then(response => response.json()).then(loadProject)
+
+
